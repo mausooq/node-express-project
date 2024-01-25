d41edba5bf30ccedfff9946ba3e1da57009a36e5
mausooq <mauchoomausooq051@gmail.com>
2024-01-25 23:12:46 +0530

    new?

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,14 @@
-
+const asyncHandler = require('express-async-handler')
 //@desc get all contact
 //@route GET api/contact
 // @access public
-exports.getContact =async (req,res) => {
+exports.getContact =asyncHandlerasync (async(req,res) => {
     res.status(200).send('get all contacts');
-}
+})
 //@desc update new contact
 //@route POST api/contact
 //@access public
-exports. createContact= async (req,res) => {
+exports. createContact=asyncHandler(async (req,res) => {
     console.log(req.body);
     const {name,email,phone} =req.body;
     if(!name ||!email || !phone){
@@ -16,22 +16,22 @@ exports. createContact= async (req,res) => {
         throw new Error("all fields filled")
     }
     res.status(201).send('create new contacts');
-}
+})
 //@desc get contact 
 //@route GET api/contact/:id
 //@access public
-exports.getContacts = async (req,res) => {
+exports.getContacts = asyncHandler(async (req,res) => {
     res.status(200).send(`get contact for ${req.params.id}`);
-}
+})
 //@desc  update contact 
 //@route PUT api/contact/:id
 //@access public
-exports.updateContact = async (req,res) => {
-    res.status(200).send(`update contact for ${req.params.id}`);
-}
+exports.updateContact = asyncHandler (async (req,res) => {
+    res.status(200).send(`update cont(act for ${req.params.id}`);
+})
 //@desc  delete  contact 
 //@route DELETE api/contact/:id
 //@access public
-exports.deleteContact = async (req,res) => {
+exports.deleteContact = asyncHandler ( async (req,res) => {
     res.status(200).send(`delete contact for ${req.params.id}`);
-}
+})
